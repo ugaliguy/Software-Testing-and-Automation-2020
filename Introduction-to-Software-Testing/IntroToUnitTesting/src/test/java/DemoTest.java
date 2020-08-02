ad2b14bcf54e0c29a3909fb80dcfe7a1a6de064d
ugaliguy <ugali_guy@hotmail.com>
2020-08-01 20:07:22 -0500

    Commented out redundant tests

--- Introduction-to-Software-Testing/IntroToUnitTesting/src/test/java/DemoTest.java
+++ Introduction-to-Software-Testing/IntroToUnitTesting/src/test/java/DemoTest.java
@@ -71,25 +71,6 @@ public class DemoTest {
 		
 		assertEquals(consoleOutput, out.toString());
 	}
-	
-//	@Test
-//	public void test_main_bad_input_1() {
-//		String input = "inf\n12\n18";
-//		ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-//		System.setIn(in);
-//		ByteArrayOutputStream out = new ByteArrayOutputStream();
-//		System.setOut(new PrintStream(out));
-//		
-//		String[] args = {};
-//		Demo.main(args);
-//		
-//		String consoleOutput = "Enter side 1: " + System.getProperty("line.separator");
-//		consoleOutput += "Enter side 2: " + System.getProperty("line.separator");
-//		consoleOutput += "Enter side 3: " + System.getProperty("line.separator");
-//		consoleOutput += "This is not a triangle." + System.getProperty("line.separator");
-//		
-//		assertEquals(consoleOutput, out.toString());
-//	}
 
 //	Test the isTriangle() method
 
@@ -123,50 +104,50 @@ public class DemoTest {
 		assertTrue(Demo.isTriangle(5, 4, 3));
 	}
 	
-	@Test
-	public void test_is_triangle_7() {
-		assertTrue(Demo.isTriangle(999999999, 888888888, 333333333));
-	}
+//	@Test
+//	public void test_is_triangle_7() {
+//		assertTrue(Demo.isTriangle(999999999, 888888888, 333333333));
+//	}
+//	
+//	@Test
+//	public void test_is_triangle_8() {
+//		assertTrue(Demo.isTriangle(2147483647, 2147483647, 2147483647));
+//	}
 	
-	@Test
-	public void test_is_triangle_8() {
-		assertTrue(Demo.isTriangle(2147483647, 2147483647, 2147483647));
-	}
+//	@Test
+//	public void test_is_triangle_double_1() {
+//		assertTrue(Demo.isTriangle(0.3, 0.4, 0.5));
+//	}
 	
-	@Test
-	public void test_is_triangle_double_1() {
-		assertTrue(Demo.isTriangle(0.3, 0.4, 0.5));
-	}
-	
-	@Test
-	public void test_is_triangle_double_2() {
-		assertTrue(Demo.isTriangle(0.3, 4.8, 5));
-	}
-	
-	@Test
-	public void test_is_triangle_double_3() {
-		assertTrue(Demo.isTriangle(0.00000000003, 4.00000000008, 4.0000000000999));
-	}
-	
-	@Test
-	public void test_is_triangle_double_4() {
-		assertTrue(Demo.isTriangle(0.0001, 99999.9999, 99999.9999));
-	}
-	
-	@Test
-	public void test_is_triangle_double_5() {
-		assertTrue(Demo.isTriangle(0.000001, 0.000001, 0.000001));
-	}
-	
-	@Test
-	public void test_is_triangle_double_6() {
-		assertTrue(Demo.isTriangle(9999999.999999, 9999999.999999, 9999999.999999));
-	}
-	
-	@Test
-	public void test_is_triangle_double_7() {
-		assertTrue(Demo.isTriangle(Math.sqrt(448317), Math.sqrt(443556), Math.sqrt(4761)));
-	}
+//	@Test
+//	public void test_is_triangle_double_2() {
+//		assertTrue(Demo.isTriangle(0.3, 4.8, 5));
+//	}
+//	
+//	@Test
+//	public void test_is_triangle_double_3() {
+//		assertTrue(Demo.isTriangle(0.00000000003, 4.00000000008, 4.0000000000999));
+//	}
+//	
+//	@Test
+//	public void test_is_triangle_double_4() {
+//		assertTrue(Demo.isTriangle(0.0001, 99999.9999, 99999.9999));
+//	}
+//	
+//	@Test
+//	public void test_is_triangle_double_5() {
+//		assertTrue(Demo.isTriangle(0.000001, 0.000001, 0.000001));
+//	}
+//	
+//	@Test
+//	public void test_is_triangle_double_6() {
+//		assertTrue(Demo.isTriangle(9999999.999999, 9999999.999999, 9999999.999999));
+//	}
+//	
+//	@Test
+//	public void test_is_triangle_double_7() {
+//		assertTrue(Demo.isTriangle(Math.sqrt(448317), Math.sqrt(443556), Math.sqrt(4761)));
+//	}
 	
 	@Test
 	public void test_is_NOT_triangle_1() {
@@ -223,6 +204,21 @@ public class DemoTest {
 		assertFalse(Demo.isTriangle(5, 4, 0));
 	}
 	
+//	@Test
+//	public void test_is_NOT_triangle_04() {
+//		assertFalse(Demo.isTriangle(0, 3, 4));
+//	}
+//	
+//	@Test
+//	public void test_is_NOT_triangle_05() {
+//		assertFalse(Demo.isTriangle(3, 0, 5));
+//	}
+//	
+//	@Test
+//	public void test_is_NOT_triangle_06() {
+//		assertFalse(Demo.isTriangle(4, 5, 0));
+//	}
+	
 	@Test
 	public void test_is_NOT_triangle_negative_1() {
 		assertFalse(Demo.isTriangle(-5, 4, 3));
@@ -240,27 +236,42 @@ public class DemoTest {
 	
 	@Test
 	public void test_is_NOT_triangle_negative_4() {
-		assertFalse(Demo.isTriangle(-5, -4, -3));
+		assertFalse(Demo.isTriangle(-5, -4, 3));
 	}
 	
 	@Test
 	public void test_is_NOT_triangle_negative_5() {
-		assertFalse(Demo.isTriangle(-1, -1, -1));
+		assertFalse(Demo.isTriangle(-5, 4, -3));
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_infinity_1() {
-		assertFalse(Demo.isTriangle(inf, 4, 3));
+	public void test_is_NOT_triangle_negative_6() {
+		assertFalse(Demo.isTriangle(5, -4, -3));
 	}
 	
 	@Test
-	public void test_is_NOT_triangle_infinity_2() {
-		assertFalse(Demo.isTriangle(inf, 4, inf));
+	public void test_is_NOT_triangle_negative_7() {
+		assertFalse(Demo.isTriangle(-5, -4, -3));
 	}
 	
-	@Test
-	public void test_is_NOT_triangle_infinity_3() {
-		assertFalse(Demo.isTriangle(inf, inf, inf));
-	}
+//	@Test
+//	public void test_is_NOT_triangle_negative_5() {
+//		assertFalse(Demo.isTriangle(-1, -1, -1));
+//	}
+	
+//	@Test
+//	public void test_is_NOT_triangle_infinity_1() {
+//		assertFalse(Demo.isTriangle(inf, 4, 3));
+//	}
+//	
+//	@Test
+//	public void test_is_NOT_triangle_infinity_2() {
+//		assertFalse(Demo.isTriangle(inf, 4, inf));
+//	}
+//	
+//	@Test
+//	public void test_is_NOT_triangle_infinity_3() {
+//		assertFalse(Demo.isTriangle(inf, inf, inf));
+//	}
 	
 }
