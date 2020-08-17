38fc2c76883c7f4ee4b4922343c3f76509f90858
ugaliguy <ugali_guy@hotmail.com>
2020-08-17 09:17:16 -0500

    Initial commit of working project

---
+++ Introduction-to-Software-Testing/CoffeeMaker_JUnit_Golden/src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright (c) 2009,  Sarah Heckman, Laurie Williams, Dright Ho
+ * All Rights Reserved.
+ * 
+ * Permission has been explicitly granted to the University of Minnesota 
+ * Software Engineering Center to use and distribute this source for 
+ * educational purposes, including delivering online education through
+ * Coursera or other entities.  
+ * 
+ * No warranty is given regarding this software, including warranties as
+ * to the correctness or completeness of this software, including 
+ * fitness for purpose.
+ * 
+ * 
+ * Modifications 
+ * 20171114 - Ian De Silva - Updated to comply with JUnit 4 and to adhere to 
+ * 							 coding standards.  Added test documentation.
+ */
+package edu.ncsu.csc326.coffeemaker;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.ncsu.csc326.coffeemaker.exceptions.InventoryException;
+import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
+
+/**
+ * Unit tests for CoffeeMaker class.
+ * 
+ * @author Sarah Heckman
+ */
+public class CoffeeMakerTest {
+	
+	/**
+	 * The object under test.
+	 */
+	private CoffeeMaker coffeeMaker;
+	
+	// Sample recipes to use in testing.
+	private Recipe recipe1;
+	private Recipe recipe2;
+	private Recipe recipe3;
+	private Recipe recipe4;
+	
+//	private CoffeeMaker cm;
+
+	/**
+	 * Initializes some recipes to test with and the {@link CoffeeMaker} 
+	 * object we wish to test.
+	 * 
+	 * @throws RecipeException  if there was an error parsing the ingredient 
+	 * 		amount when setting up the recipe.
+	 */
+	@Before
+	public void setUp() throws RecipeException {
+		coffeeMaker = new CoffeeMaker();
+		
+		//Set up for r1
+		recipe1 = new Recipe();
+		recipe1.setName("Coffee");
+		recipe1.setAmtChocolate("0");
+		recipe1.setAmtCoffee("3");
+		recipe1.setAmtMilk("1");
+		recipe1.setAmtSugar("1");
+		recipe1.setPrice("50");
+		
+		//Set up for r2
+		recipe2 = new Recipe();
+		recipe2.setName("Mocha");
+		recipe2.setAmtChocolate("20");
+		recipe2.setAmtCoffee("3");
+		recipe2.setAmtMilk("1");
+		recipe2.setAmtSugar("1");
+		recipe2.setPrice("75");
+		
+		//Set up for r3
+		recipe3 = new Recipe();
+		recipe3.setName("Latte");
+		recipe3.setAmtChocolate("0");
+		recipe3.setAmtCoffee("3");
+		recipe3.setAmtMilk("3");
+		recipe3.setAmtSugar("1");
+		recipe3.setPrice("100");
+		
+		//Set up for r4
+		recipe4 = new Recipe();
+		recipe4.setName("Hot Chocolate");
+		recipe4.setAmtChocolate("4");
+		recipe4.setAmtCoffee("0");
+		recipe4.setAmtMilk("1");
+		recipe4.setAmtSugar("1");
+		recipe4.setPrice("65");
+	}
+	
+	
+	/**
+	 * Given a coffee maker with the default inventory
+	 * When we add inventory with well-formed quantities
+	 * Then we do not get an exception trying to read the inventory quantities.
+	 * 
+	 * @throws InventoryException  if there was an error parsing the quanity
+	 * 		to a positive integer.
+	 */
+	@Test
+	public void testAddInventory() throws InventoryException {
+		coffeeMaker.addInventory("4","7","0","9");
+	}
+	
+	/**
+	 * Given a coffee maker with the default inventory
+	 * When we add inventory with malformed quantities (i.e., a negative 
+	 * quantity and a non-numeric string)
+	 * Then we get an inventory exception
+	 * 
+	 * @throws InventoryException  if there was an error parsing the quanity
+	 * 		to a positive integer.
+	 */
+	@Test(expected = InventoryException.class)
+	public void testAddInventoryException() throws InventoryException {
+		coffeeMaker.addInventory("4", "-1", "asdf", "3");
+	}
+	
+	/**
+	 * Given a coffee maker with one valid recipe
+	 * When we make coffee, selecting the valid recipe and paying more than 
+	 * 		the coffee costs
+	 * Then we get the correct change back.
+	 */
+	@Test
+	public void testMakeCoffee() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(25, coffeeMaker.makeCoffee(0, 75));
+	}
+	
+	@Test
+	public void testAddRecipe1() {
+		Recipe temp;
+		boolean contains = false;
+		coffeeMaker.addRecipe(recipe1);
+		Recipe[] array = coffeeMaker.getRecipes();
+		for (int i = 0; i < array.length; i++) {
+			temp = array[i];
+			if (recipe1 == temp) {
+				contains = true;
+			}
+		}
+		assertTrue("Recipe not added.",contains);
+	}
+	
+	@Test
+	public void testAddRecipe1F() {
+		Recipe temp;
+		boolean contains = false;
+		coffeeMaker.addRecipe(recipe1);
+		Recipe[] array = coffeeMaker.getRecipes();
+		for (int i = 0; i < array.length; i++) {
+			temp = array[i];
+			if (recipe1 == temp) {
+				contains = true;
+			}
+		}
+		assertFalse("Recipe not added.",contains);
+	}
+	
+//	@Test
+//	public void testDeleteRecipe() {
+//		Recipe temp;
+//		boolean contains = false;
+//		coffeeMaker.addRecipe(recipe1);
+//		contains = false;
+//		//R1 will be the recipe in slot 0, because it was the first (and only) one added.
+//		String name = coffeeMaker.deleteRecipe(0);
+//		Recipe[] array_deleted = coffeeMaker.getRecipes();
+//		for (Recipe recipe : array_deleted) {
+//			if (recipe != null && recipe.getName() != "") {
+//				contains = true;
+//			}
+//		}
+//		assertFalse("Recipe was not deleted.",contains);
+//		assertTrue("Wrong name returned while deleting recipe.",recipe1.getName().equals(name));
+//		
+//		assertNull("Null was not returned while deleting a recipe that did not exist.",coffeeMaker.deleteRecipe(1));
+//		assertEquals("Incorrect return value while deleting a recipe that was already deleted.",
+//				"", coffeeMaker.deleteRecipe(0)); //TODO This recipe was already deleted. Should it be null instead of ""?
+//	}
+
+}
